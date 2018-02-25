b9d61f412f0acba815dfc748b5067464189459a2
Nerses <nerses.martirosyan@gmail.com>
2018-02-24 18:04:35 -0800

    created lab3

--- lab2/app.js
+++ lab2/app.js
@@ -20,19 +20,29 @@ class PubSub {
     }
 }
 
-const pubSub = new PubSub();
+	//new PUBSUB
+	const pubSub = new PubSub();
 
-
-	var testB = document.getElementById('mainB');
+	//get the main create button
+	var testB = document.getElementById('incrementButton');
+	//assign button to listen to changes from pubsub
 	testB.addEventListener('click', function(){
 		pubSub.publish(window.incrementalGame.state.counter);
 	});
 
-	pubSub.subscribe(action => {
+	pubSub.subscribe(function(){
+		//listen to click
 		var temp = parseInt(document.getElementById('score').innerHTML);
-		console.log(temp);
-		temp ++;
+
+		//increment the main counter
+		window.incrementalGame.state.counter ++;
+		temp = window.incrementalGame.state.counter;
+
+		//set the DOM(inner html) to become same as main counter
 		document.getElementById('score').innerHTML = temp;
+
+		//printing for testing
+		console.log(window.incrementalGame.state.counter);
 	});
 
 ///function that increments the counter goes inside subscribers
