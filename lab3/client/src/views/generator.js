24103d8bcbbaaff87012ca20cfb8fef4dc4357f2
Nerses <nerses.martirosyan@gmail.com>
2018-03-10 15:14:27 -0800

    Finished view-Generators

--- lab3/client/src/views/generator.js
+++ lab3/client/src/views/generator.js
@@ -5,32 +5,45 @@ export default function (store) {
 			this.store = store;
 
 			// TODO: render generator initial view
-			this.textContent =  this.store.state.example;
+			this.textContent = this.store.state.example;
 
-			// TODO: subscribe to store on change event
-			store.subscribe(function(){
-				//listen to click
-				var temp = parseInt(document.getElementById('score').innerHTML);
-
-				//increment the main counter
-				window.incrementalGame.state.counter ++;
-				temp = window.incrementalGame.state.counter;
-
-				//set the DOM(inner html) to become same as main counter
-				document.getElementById('score').innerHTML = temp;
-
-				//printing for testing
-				console.log(window.incrementalGame.state.counter);
-			});				
+			this.onStateChange = this.handleStateChange.bind(this);
 
 
 			// TODO: add click event
 			this.addEventListener('click', () => {
 				this.store.dispatch({
-					type: 'EXAMPLE_MUTATION',
-					payload: 'You clicked this element'
+					type: 'BUY_GENERATOR',
+					payload: 'Bought Generator'
 				});
 			});
+		}
+
+		handleStateChange (newState) {
+			console.log('StateChange', this);
+			this.textContent = newState.example;
+		}
+
+
+		// TODO: subscribe to store on change event
+		connectedCallback () {
+			console.log('ConnectedCallback');
+			this.innerHTML = `
+				<div id=a>
+	            	<label id=Desc>Name</label>
+	            	<p> Description </p>
+	            	<label>0/0</label>
+	            	<button id=cost > {cost} Resources</button>
+	       		 </div>`;
+
+			this.store.subscribe(this.onStateChange);
+		}
+
+
+
+		disconnectedCallback () {
+			console.log('DisconnectedCallback');
+			this.store.unsubscribe(this.onStateChange);
 		}
 	};
 }
